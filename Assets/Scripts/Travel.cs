3034c8845181a4aa7b206e67b972e28c9484196e
mareRong <156254766+mareRong@users.noreply.github.com>
2026-05-06 20:17:19 -0700

    updated speed

--- Assets/Scripts/Travel.cs
+++ Assets/Scripts/Travel.cs
@@ -9,9 +9,9 @@ public class Travel : MonoBehaviour
     public Transform drone;
 
     [Header("Speeds")]
-    public float moveSpeed = 35f;
-    public float verticalSpeed = 20f;
-    public float rotationSpeed = 300f;
+    public float moveSpeed = 45f;
+    public float verticalSpeed = 35f;
+    public float rotationSpeed = 140f;
 
     [Header("Right Hand Thresholds")]
     public float fistThreshold = 0.075f;
